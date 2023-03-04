0aabacbe2e7a8289e4c0522304c733eccfd58d87
LaurensDM <laurens.demaeyer@student.hogent.be>
2023-03-04 12:22:14 +0100

    small fix

--- src/data/seeds/202302171500_order.js
+++ src/data/seeds/202302171500_order.js
@@ -8,18 +8,10 @@ module.exports = {
         currencyId: "EUR",
         orderReference: "REF1",
         orderDateTime: "Fri Mar 03 2023 15:34:55 GMT+0100 (Central European Standard Time)",
-<<<<<<< HEAD
-        netAmount: 49.99,
-        taxAmount: 0,
-        totalAmount: 49.99,
-        currencyId: "EUR",
-      },
-=======
         netPrice: 49.99,
         taxPrice: 0,
         totalPrice: 49.99,
-      }
->>>>>>> 4d1be03 (Adjusting existing seeds, creating new seeds)
+      },
     ]);
   },
 };
