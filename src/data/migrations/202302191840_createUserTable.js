bb591a479b90f2432ad63d141273ba82c1ffaf86
LaurensDM <laurens.demaeyer@student.hogent.be>
2023-03-05 16:36:27 +0100

    Add adress to user

--- src/data/migrations/202302191840_createUserTable.js
+++ src/data/migrations/202302191840_createUserTable.js
@@ -6,8 +6,13 @@ module.exports = {
   up: async (knex) => {
     await knex.schema.createTable(tables.user, (table) => {
       table.uuid('id').primary().unique();
-      table.string('name', 255)
+      table.string('name', 64)
         .defaultTo('anon');
+      table.string('street', 64);
+      table.string('streetNumber', 16);
+      table.string('zipCode', 16);
+      table.string('city', 64);
+      table.string('country', 32);
       table.string('email', 255)
         .notNullable()
         .unique();
