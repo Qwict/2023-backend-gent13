826fe9c3e24e9d74b7ab8c1d2ac88d7ed53bd118
Alexander Schatteman <alexander.schatteman@student.hogent.be>
2023-03-06 09:54:17 +0100

    product change

--- src/repository/product.js
+++ src/repository/product.js
@@ -1,7 +1,11 @@
 const { tables, getKnex } = require('../data');
 
 async function findById(id) {
-  const product = await getKnex()(tables.product).where('id', id).first();
+  const product = await getKnex()(tables.product)
+    .join(tables.productPrice, `${tables.productPrice}`.userId, '=', `${tables.product}`.id)
+    .join(tables.productDescription, `${tables.productDescription}`.productId, '=', `${tables.product}`.id)
+    .where(`${tables.product}.id`, id)
+    .first();
   return product;
 }
 
@@ -10,33 +14,7 @@ const findAll = async () => {
   return products;
 };
 
-const findPriceById = async (id) => {
-  const product = await getKnex()(tables.productPrice)
-    .select(` ${tables.productPrice}.productId`, ` ${tables.productPrice}.price`, ` ${tables.productPrice}.currencyId`, ` ${tables.productPrice}.quantity`)
-    .join(tables.product, ` ${tables.product}.id`, '=', `${tables.productPrice}.productId`)
-    .where(`${tables.product}.id`, id)
-    .first();
-  return product;
-};
-
-const findDescriptionById = async (id) => {
-  const product = await getKnex()(tables.productDescription)
-    .select(
-      ` ${tables.productDescription}.productId`,
-      ` ${tables.productDescription}.productName`,
-      ` ${tables.productDescription}.productShortDescription`,
-      ` ${tables.productDescription}.productLongDescription`,
-    )
-    .join(tables.product, ` ${tables.product}.id`, '=', `${tables.productDescription}.productId`)
-    .where(tables.productDescription.id, id)
-    .first();
-
-  return product;
-};
-
 module.exports = {
   findById,
   findAll,
-  findPriceById,
-  findDescriptionById,
 };
