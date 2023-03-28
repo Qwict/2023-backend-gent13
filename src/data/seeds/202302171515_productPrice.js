5551f8bf64480dee6a50e4bc73f84cabe7242068
LaurensDM <laurens.demaeyer@student.hogent.be>
2023-03-28 15:15:00 +0200

    seeds

--- src/data/seeds/202302171515_productPrice.js
+++ src/data/seeds/202302171515_productPrice.js
@@ -5,32 +5,39 @@ module.exports = {
     await knex(tables.productPrice).insert([
       {
         productId: 4,
-        price: 215.0,
+        price: 215.99,
         quantity: 1,
       },
+
       {
         productId: 5,
-        price: 219.0,
+        price: 219.99,
         quantity: 1,
       },
+
       {
         productId: 6,
+
         price: 92.99,
         quantity: 1,
       },
+
       {
         productId: 7,
-        price: 809.0,
+        price: 809.59,
         quantity: 1,
       },
+
       {
         productId: 8,
-        price: 579.0,
+
+        price: 579.49,
         quantity: 1,
       },
+
       {
         productId: 9,
-        price: 1145.0,
+        price: 1147.99,
         quantity: 1,
       },
       {
@@ -38,14 +45,16 @@ module.exports = {
         price: 1969.0,
         quantity: 1,
       },
+
       {
         productId: 11,
-        price: 1889.0,
+        price: 1889.49,
         quantity: 1,
       },
+
       {
         productId: 12,
-        price: 1089.0,
+        price: 1089.09,
         quantity: 1,
       },
 
@@ -57,27 +66,30 @@ module.exports = {
 
       {
         productId: 14,
-        price: 2199.0,
+        price: 2199.89,
         quantity: 1,
       },
 
       {
         productId: 15,
-        price: 1599.0,
+
+        price: 1599.49,
         quantity: 3,
       },
 
       {
         productId: 16,
-        price: 1699.0,
+        price: 1699.49,
         quantity: 1,
       },
 
       {
         productId: 17,
-        price: 1309.0,
+
+        price: 1309.39,
         quantity: 1,
       },
+
       {
         productId: 18,
         price: 136.99,
@@ -92,7 +104,7 @@ module.exports = {
 
       {
         productId: 20,
-        price: 10000.0,
+        price: 29.99,
         quantity: 1,
       },
 
